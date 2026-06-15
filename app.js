d5322d6c839ca80cca02ab4d08ca714f9c973c33
Sakshi Shah <sakshishah2406@gmail.com>
2026-06-15 17:21:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let present = 0;
+let absent = 0;
+
+function updateUI(){
+
+ let total = present + absent;
+
+ let percentage = 0;
+
+ if(total > 0){
+   percentage = (present / total) * 100;
+ }
+
+ document.getElementById("present").innerText = present;
+ document.getElementById("absent").innerText = absent;
+ document.getElementById("percentage").innerText =
+ percentage.toFixed(2) + "%";
+}
+
+function markPresent(){
+ present++;
+ updateUI();
+}
+
+function markAbsent(){
+ absent++;
+ updateUI();
+}
+
+updateUI();
